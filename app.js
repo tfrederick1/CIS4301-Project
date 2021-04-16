3c1ef8f598cac872f9a5ea9a32f1ab6f9d44f999
tfrederick1 <60411410+tfrederick1@users.noreply.github.com>
2021-04-16 09:36:51 -0400

    Updated Query 2, Added Query 3/4

--- app.js
+++ app.js
@@ -68,11 +68,16 @@ io.on("connection", (socket) => {
     }
 
     let se= run(arguments);
-    //Wait for 5 mins
     se.then(value => io.emit("Results",value));
   });
+
+
+
+
   socket.on('Query 2',(arguments) => {
     console.log(arguments);
+    
+    // Instant client handler. Verifies that instant client can be found
     async function run(arguments) {
       let connection;
 
@@ -84,28 +89,67 @@ io.on("connection", (socket) => {
         });
 
         const result = await connection.execute(
-            `SELECT EXTRACT(MONTH FROM END_TIME) AS month, COUNT(*) AS COUNT
-            FROM PROJECT_FLIGHT
-            WHERE DEPARTURE  IN
-            (SELECT ICAOAP FROM PROJECT_AIRPORT WHERE COUNTRY IN
+            `SELECT X1, Y1, Y2, Y3, Y4 FROM
             (
-            SELECT DISTINCT COUNTRY_NAME AS HOTSPOTS FROM(
-            SELECT COUNTRY_NAME, MONTH, SUMS, ROW_NUMBER() OVER(PARTITION BY MONTH ORDER BY SUMS DESC) AS RN
+            SELECT M1 AS X1, C1+C2 AS Y1 FROM
+            (SELECT EXTRACT(MONTH FROM END_TIME) M1, A1.COUNTRY, COUNT(*) C1
+            FROM "PROJECT_FLIGHT" JOIN "PROJECT_AIRPORT" A1 ON DEPARTURE = A1.ICAOAP
+            WHERE A1.COUNTRY = 'INDIA'
+            GROUP  BY  EXTRACT(MONTH FROM END_TIME), A1.COUNTRY
+            ORDER BY EXTRACT(MONTH FROM END_TIME)
+            ) JOIN
+            (
+            SELECT EXTRACT(MONTH FROM END_TIME) M2, A2.COUNTRY, COUNT(*) C2
+            FROM "PROJECT_FLIGHT" JOIN "PROJECT_AIRPORT" A2 ON DESTINATION = A2.ICAOAP
+            WHERE A2.COUNTRY = 'INDIA'
+            GROUP  BY  EXTRACT(MONTH FROM END_TIME), A2.COUNTRY
+            ORDER BY EXTRACT(MONTH FROM END_TIME)
+            )
+            ON M1 = M2
+            )
+            NATURAL JOIN
+            (
+            SELECT EXTRACT(MONTH FROM DAY) AS X1, SUM(NEW_CASES) AS Y2
+            FROM "PROJECT_STATISTIC2020"
+            WHERE COUNTRY_NAME = 'INDIA'
+            GROUP  BY  EXTRACT(MONTH FROM DAY)
+            ORDER BY EXTRACT(MONTH FROM DAY)
+            )
+            JOIN
+            (
+            SELECT M1 AS X2, ROUND(CNT1+CNT2, 4) AS Y3 FROM
+            (
+            SELECT EXTRACT(MONTH FROM D2) M2, (COUNT(*)*13800/P2) CNT2
             FROM(
-            SELECT COUNTRY_NAME, EXTRACT(MONTH FROM DAY) AS MONTH, SUM(NEW_CASES) AS SUMS
-            FROM PROJECT_STATISTIC
-            WHERE COUNTRY_NAME NOT IN ('WORLD', 'EUROPE', 'NORTH AMERICA', 'ASIA', 'AFRICA', 'NORTH AMERICA', 'EUROPEAN UNION', 'SOUTH AMERICA') AND NEW_CASES IS NOT NULL
-            GROUP BY COUNTRY_NAME, EXTRACT(MONTH FROM DAY)
-            ORDER BY MONTH DESC, SUM(NEW_CASES) DESC
+            SELECT END_TIME D2, PC2.POPULATION P2, A2.COUNTRY
+            FROM "PROJECT_FLIGHT" JOIN "PROJECT_AIRPORT" A2 ON DESTINATION = A2.ICAOAP JOIN "PROJECT_COUNTRY" PC2 ON A2.COUNTRY = PC2.NAME
+            WHERE A2.COUNTRY = 'INDIA'
             )
+            GROUP BY EXTRACT(MONTH FROM D2), P2
+            ORDER BY EXTRACT(MONTH FROM D2)
             )
-            WHERE RN <=5
+            JOIN
+            (
+            SELECT EXTRACT(MONTH FROM D1) M1, (COUNT(*)*13800/P1) CNT1
+            FROM(
+            SELECT END_TIME D1, PC1.POPULATION P1, A1.COUNTRY
+            FROM "PROJECT_FLIGHT" JOIN "PROJECT_AIRPORT" A1 ON DEPARTURE = A1.ICAOAP JOIN "PROJECT_COUNTRY" PC1 ON A1.COUNTRY = PC1.NAME
+            WHERE A1.COUNTRY = 'INDIA'
             )
+            GROUP BY EXTRACT(MONTH FROM D1), P1
+            ORDER BY EXTRACT(MONTH FROM D1)
             )
-            AND DESTINATION IN
-            (SELECT ICAOAP FROM PROJECT_AIRPORT WHERE COUNTRY = '` + arguments[0] + `')
-            GROUP BY  EXTRACT(MONTH FROM END_TIME)
-            ORDER BY EXTRACT(MONTH FROM END_TIME)`
+            ON M1 = M2
+            )
+            ON X1 = X2
+            NATURAL JOIN
+            (
+            SELECT EXTRACT(MONTH FROM DAY) AS X2, ROUND(100 *SUM(NEW_CASES)/ (SELECT POPULATION FROM "PROJECT_COUNTRY" WHERE NAME = 'INDIA'), 8) AS Y4
+            FROM "PROJECT_STATISTIC2020"
+            WHERE COUNTRY_NAME = 'INDIA'
+            GROUP  BY  EXTRACT(MONTH FROM DAY)
+            ORDER BY EXTRACT(MONTH FROM DAY)
+            )`
         );
         console.log(result.rows);
         return result.rows;
@@ -123,9 +167,12 @@ io.on("connection", (socket) => {
     }
 
     let se= run(arguments);
-    //Wait for 5 mins
-    se.then(value => io.emit("Results",value));
+    se.then(value => io.emit("Query 2",value));
   });
+
+
+
+
   socket.on('Query 3',(arguments) => {
     console.log(arguments);
     async function run(arguments) {
@@ -139,10 +186,30 @@ io.on("connection", (socket) => {
         });
 
         const result = await connection.execute(
-            `SELECT *
-            FROM "PROJECT_AIRPORT"
-            FETCH FIRST 10 ROWS ONLY`
+          `SELECT EXTRACT(MONTH FROM END_TIME) AS month, COUNT(*) AS COUNT
+          FROM PROJECT_FLIGHT
+          WHERE DEPARTURE  IN
+          (SELECT ICAOAP FROM PROJECT_AIRPORT WHERE COUNTRY IN
+          (
+          SELECT DISTINCT COUNTRY_NAME AS HOTSPOTS FROM(
+          SELECT COUNTRY_NAME, MONTH, SUMS, ROW_NUMBER() OVER(PARTITION BY MONTH ORDER BY SUMS DESC) AS RN
+          FROM(
+          SELECT COUNTRY_NAME, EXTRACT(MONTH FROM DAY) AS MONTH, SUM(NEW_CASES) AS SUMS
+          FROM PROJECT_STATISTIC
+          WHERE COUNTRY_NAME NOT IN ('WORLD', 'EUROPE', 'NORTH AMERICA', 'ASIA', 'AFRICA', 'NORTH AMERICA', 'EUROPEAN UNION', 'SOUTH AMERICA') AND NEW_CASES IS NOT NULL
+          GROUP BY COUNTRY_NAME, EXTRACT(MONTH FROM DAY)
+          ORDER BY MONTH DESC, SUM(NEW_CASES) DESC
+          )
+          )
+          WHERE RN <=5
+          )
+          )
+          AND DESTINATION IN
+          (SELECT ICAOAP FROM PROJECT_AIRPORT WHERE COUNTRY = '` + arguments[0] + `')
+          GROUP BY  EXTRACT(MONTH FROM END_TIME)
+          ORDER BY EXTRACT(MONTH FROM END_TIME)`
         );
+        console.log(result.rows);
         return result.rows;
       } catch(err) {
         console.error(err);
@@ -158,9 +225,12 @@ io.on("connection", (socket) => {
     }
 
     let se= run(arguments);
-    //Wait for 5 mins
-    se.then(value => io.emit("Results",value));
+    se.then(value => io.emit("Query 3",value));
   });
+
+
+
+
   socket.on('Query 4',(arguments) => {
     console.log(arguments);
     async function run(arguments) {
@@ -174,10 +244,74 @@ io.on("connection", (socket) => {
         });
 
         const result = await connection.execute(
-            `SELECT *
-            FROM "PROJECT_AIRPORT"
-            FETCH FIRST 10 ROWS ONLY`
+            `SELECT X1, Y1, Y2, Y3
+            FROM
+            (
+            SELECT X1, ROUND(C/
+            (SELECT MAX(C) FROM
+            (SELECT MONTH X1, COUNT(*) C FROM(
+            SELECT EXTRACT(MONTH FROM END_TIME) AS MONTH ,SUBSTR(CALLSIGN,1,3) AS ICAO
+            FROM PROJECT_FLIGHT
+            )
+            WHERE ICAO IN (SELECT ICAOAL FROM PROJECT_AIRLINE WHERE NAME = '`+ arguments[0] +`')
+            GROUP BY MONTH
+            ORDER BY MONTH)
+            ),4) Y1
+            FROM (
+            SELECT MONTH X1, COUNT(*) C FROM(
+            SELECT EXTRACT(MONTH FROM END_TIME) AS MONTH ,SUBSTR(CALLSIGN,1,3) AS ICAO
+            FROM PROJECT_FLIGHT
+            )
+            WHERE ICAO IN (SELECT ICAOAL FROM PROJECT_AIRLINE WHERE NAME = '`+ arguments[0] +`')
+            GROUP BY MONTH
+            ORDER BY MONTH)
+            )
+            JOIN
+            (
+            SELECT X2, ROUND(C/
+            (SELECT MAX(C) FROM
+            (SELECT MONTH X2, COUNT(*) C FROM(
+            SELECT EXTRACT(MONTH FROM END_TIME) AS MONTH ,SUBSTR(CALLSIGN,1,3) AS ICAO
+            FROM PROJECT_FLIGHT
+            )
+            WHERE ICAO IN (SELECT ICAOAL FROM PROJECT_AIRLINE WHERE NAME = '`+ arguments[1] +`')
+            GROUP BY MONTH
+            ORDER BY MONTH)
+            ),4) Y2
+            FROM (
+            SELECT MONTH X2, COUNT(*) C FROM(
+            SELECT EXTRACT(MONTH FROM END_TIME) AS MONTH ,SUBSTR(CALLSIGN,1,3) AS ICAO
+            FROM PROJECT_FLIGHT
+            )
+            WHERE ICAO IN (SELECT ICAOAL FROM PROJECT_AIRLINE WHERE NAME = '`+ arguments[1] +`')
+            GROUP BY MONTH
+            ORDER BY MONTH)
+            )
+            ON X1 = X2
+            JOIN
+            (
+            SELECT X3, ROUND(C/
+            (SELECT MAX(C) FROM
+            (SELECT MONTH X3, COUNT(*) C FROM(
+            SELECT EXTRACT(MONTH FROM END_TIME) AS MONTH ,SUBSTR(CALLSIGN,1,3) AS ICAO
+            FROM PROJECT_FLIGHT
+            )
+            WHERE ICAO IN (SELECT ICAOAL FROM PROJECT_AIRLINE WHERE NAME = '`+ arguments[2] +`')
+            GROUP BY MONTH
+            ORDER BY MONTH)
+            ),4) Y3
+            FROM (
+            SELECT MONTH X3, COUNT(*) C FROM(
+            SELECT EXTRACT(MONTH FROM END_TIME) AS MONTH ,SUBSTR(CALLSIGN,1,3) AS ICAO
+            FROM PROJECT_FLIGHT
+            )
+            WHERE ICAO IN (SELECT ICAOAL FROM PROJECT_AIRLINE WHERE NAME = '`+ arguments[2] +`')
+            GROUP BY MONTH
+            ORDER BY MONTH)
+            )
+            ON X2 = X3`
         );
+        console.log(result.rows);
         return result.rows;
       } catch(err) {
         console.error(err);
@@ -193,9 +327,12 @@ io.on("connection", (socket) => {
     }
 
     let se= run(arguments);
-    //Wait for 5 mins
-    se.then(value => io.emit("Results",value));
+    se.then(value => io.emit("Query 4",value));
   });
+
+
+
+
   socket.on('Query 5',(arguments) => {
     console.log(arguments);
     async function run(arguments) {
@@ -228,8 +365,7 @@ io.on("connection", (socket) => {
     }
 
     let se= run(arguments);
-    //Wait for 5 mins
-    se.then(value => io.emit("Results",value));
+    se.then(value => io.emit("Query 5",value));
   });
 });
 
